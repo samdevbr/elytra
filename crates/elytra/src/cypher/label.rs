116ecdba4343a54e08c17d2414fc4b1091913123
Samuel Oliveira <samueldevbr@gmail.com>
2025-04-13 12:07:56 -0300

    Implementing `Label` doctest

--- crates/elytra/src/cypher/label.rs
+++ crates/elytra/src/cypher/label.rs
@@ -5,9 +5,20 @@ pub struct Label {
     ident: String,
 }
 
+impl Label {
+    pub fn new<I>(ident: I) -> Self
+    where
+        I: AsRef<str>,
+    {
+        Self {
+            ident: ident.as_ref().to_string(),
+        }
+    }
+}
+
 impl ToCypher for Label {
     fn to_cypher(self) -> String {
-        self.ident.to_owned()
+        self.ident
     }
 }
 
@@ -15,6 +26,17 @@ impl<T> ToCypher for T
 where
     T: Iterator<Item = Label>,
 {
+    ///
+    /// ```rust
+    /// use elytra::cypher::{ToCypher, Label};
+    ///
+    /// let label_expr = vec![Label::new("Person")];
+    /// assert_eq!(":Person", label_expr.into_iter().to_cypher());
+    ///
+    /// let label_expr = vec![Label::new("Person"), Label::new("Employee")];
+    /// assert_eq!(":Person:Employee", label_expr.into_iter().to_cypher());
+    /// ```
+    ///
     fn to_cypher(self) -> String {
         let labels = self.map(|l| l.to_cypher()).collect::<Vec<_>>();
 
