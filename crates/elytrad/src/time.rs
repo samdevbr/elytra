940cf42c9e354b1d4459f35183f43cd40dc0c76d
Samuel Oliveira <samueldevbr@gmail.com>
2025-04-24 19:02:42 -0300

    creating fn to return unix timestamp

--- crates/elytrad/src/time.rs
+++ crates/elytrad/src/time.rs
@@ -3,8 +3,10 @@ use std::{
     time::{Duration, SystemTime},
 };
 
+/// Epoch offset in seconds
 pub const EPOCH_OFFSET: u64 = 1_735_689_600;
 
+/// Local epoch for snowflakes
 pub static EPOCH: LazyLock<SystemTime> =
     LazyLock::new(|| SystemTime::UNIX_EPOCH + Duration::from_secs(EPOCH_OFFSET));
 
@@ -14,4 +16,12 @@ pub fn now() -> u64 {
         .duration_since(*EPOCH)
         .expect("clock error: time went backwards")
         .as_millis() as u64
+}
+
+/// Seconds elapsed since unix epoch
+pub fn unix_now() -> u64 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("clock error: time went backwards")
+        .as_secs()
 }
