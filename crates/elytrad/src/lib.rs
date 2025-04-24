5e720d07f81070248ad9a1260ec06c2c85503faa
Samuel Oliveira <samueldevbr@gmail.com>
2025-04-24 19:01:58 -0300

    creating a global node id register

--- crates/elytrad/src/lib.rs
+++ crates/elytrad/src/lib.rs
@@ -1,2 +1,18 @@
+use std::sync::OnceLock;
+
+pub mod document;
 pub mod id;
+pub mod shard;
 pub mod time;
+
+static NODE_ID: OnceLock<u8> = OnceLock::new();
+
+pub fn node_id() -> u8 {
+    *NODE_ID.get().expect("node id not set")
+}
+
+pub fn set_node_id(id: u8) {
+    assert!(id < 0x1F, "cannot set node id: value out of bounds");
+
+    NODE_ID.set(id).expect("node id is already set");
+}
