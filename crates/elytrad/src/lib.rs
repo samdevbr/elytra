3212bea7c9c11ccb642bc501cfa917d28092688b
Samuel Oliveira <samueldevbr@gmail.com>
2025-04-24 21:23:44 -0300

    creating trait to hash slices to u64

--- crates/elytrad/src/lib.rs
+++ crates/elytrad/src/lib.rs
@@ -1,5 +1,8 @@
 use std::sync::OnceLock;
 
+use xxhash_rust::xxh64::xxh64;
+
+pub mod document;
 pub mod field;
 pub mod id;
 pub mod key;
@@ -16,4 +19,17 @@ pub fn set_node_id(id: u8) {
     assert!(id < 0x1F, "cannot set node id: value out of bounds");
 
     NODE_ID.set(id).expect("node id is already set");
+}
+
+trait Hashable {
+    fn hash64(&self) -> u64;
+}
+
+impl<T> Hashable for T
+where
+    T: AsRef<[u8]>,
+{
+    fn hash64(&self) -> u64 {
+        xxh64(self.as_ref(), 0)
+    }
 }
