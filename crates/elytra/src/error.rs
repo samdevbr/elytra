fc2daf91a0dfd3dcf99dbfb798d99cdd0e088874
Samuel Oliveira <samueldevbr@gmail.com>
2025-04-25 21:15:24 -0300

    adding more error types

--- crates/elytra/src/error.rs
+++ crates/elytra/src/error.rs
@@ -7,4 +7,10 @@ pub enum Error {
 
     #[error(transparent)]
     Sled(#[from] sled::Error),
+
+    #[error(transparent)]
+    BinodeEncode(#[from] bincode::error::EncodeError),
+
+    #[error(transparent)]
+    Base62Decode(#[from] base62::DecodeError),
 }
